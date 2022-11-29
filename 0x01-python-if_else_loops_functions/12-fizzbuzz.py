792649b153a43c8e9788a0c761f96698b9397419
JeremyWarui <jerywarne7@gmail.com>
2022-11-29 17:11:08 +0300

    "fix fizzbuzz"

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(0, 101):
+    for num in range(1, 101):
         if num != 100:
             if num % 5 == 0 and num % 3 == 0:
                 string = "FizzBuzz"
-            elif num % 3 == 0:
-                string = "Fizz"
             elif num % 5 == 0:
                 string = "Buzz"
+            elif num % 3 == 0:
+                string = "Fizz"
             else:
                 string = num
             print("{} ".format(string), end="")

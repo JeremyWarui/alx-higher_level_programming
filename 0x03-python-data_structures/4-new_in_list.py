157534bb06dc86c7c02a0abca15926adc44b1eeb
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 07:39:51 +0000

    new list to cater for empty list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """
+    function to add new element in a given index
+    """
     copy = my_list[:]
-    if idx < 0:
+    if not my_list:
+        pass
+    elif idx < 0:
         return copy
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return copy
     else:
         copy[idx] = element

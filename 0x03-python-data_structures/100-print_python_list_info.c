bec5012beba9215e1ce8ea456e22f15b1074d18f
JeremyWarui <jerywarne7@gmail.com>
2022-12-05 22:01:06 +0300

    CPython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,10 +14,10 @@ void print_python_list_info(PyObject *p)
 	PyObject *item;
 
 	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Size of the Python List = %ld\n", size);
 
 	list = (PyListObject *)p;
-	printf("[*] Allocated = %d\n", list->allocated);
+	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{

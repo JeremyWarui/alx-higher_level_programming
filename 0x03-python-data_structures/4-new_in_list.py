968cb778b7baf307ab7a0b707985194d0fd6b66b
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 08:24:22 +0000

    fix new in list errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,12 +4,6 @@ def new_in_list(my_list, idx, element):
     function to add new element in a given index
     """
     copy = my_list[:]
-    if not my_list:
-        pass
-    elif idx <= 0:
-        return copy
-    elif idx > len(my_list):
-        return copy
-    else:
+    if 0 <= idx < len(my_list):
         copy[idx] = element
         return copy

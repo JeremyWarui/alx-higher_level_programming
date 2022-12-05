c327bb6e8449f194ee7628b8f9873205de26e0bb
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 06:51:44 +0000

    get element at given index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    """
+    function to get the element at a given index
+    """
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
         return None
     else:
         return my_list[idx]

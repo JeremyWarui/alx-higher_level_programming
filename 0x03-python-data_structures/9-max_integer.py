1c813db1076a6b2c326842f16f496a6ab299ad65
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 08:43:52 +0000

    [fix] Max integer correction

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,12 +3,12 @@ def max_integer(my_list=[]):
     """
     function to return the max integer in the list
     """
-    nums = len(my_list)
-    max_num = 0
-    if nums == 0:
+    nlen = len(my_list)
+    max_num = my_list[0]
+    if nlen == 0:
         return None
     else:
-        for num in range(1, nums):
-            if my_list[num] > max_num:
-                max_num = my_list[num]
+        for num in my_list:
+            if num >= max_num:
+                max_num = num
     return max_num

d4a76e8b1f30fba1a29a2f844b16ad2b189e12c4
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 08:31:51 +0000

    fix check errors on print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    """
+    function to print a matrix
+    """
     for i in matrix:
         for j in range(len(i)):
             if j == (len(i) - 1):
-                print("{}".format(i[j]), end="")
+                print("{:d}".format(i[j]), end="")
             else:
-                print("{} ".format(i[j]), end="")
+                print("{:d} ".format(i[j]), end="")
         print()

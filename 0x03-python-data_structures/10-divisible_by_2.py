86b1755ec9270074c87767b6b2063c10fe952634
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 08:48:35 +0000

    divisible by two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     copy = []
-    for n in my_list:
-        if n % 2 is 0:
+    for num in my_list:
+        if num % 2 == 0:
             copy.append(True)
         else:
             copy.append(False)

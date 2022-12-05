58c579b83248cce7078ff8d958e15524bd8741b5
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 07:35:05 +0000

    check if mylist is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,9 @@ def print_reversed_list_integer(my_list=[]):
     """
     function to reverse list
     """
-    my_list.reverse()
-    for num in my_list:
-        print("{:d}".format(num))
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for num in my_list:
+            print("{:d}".format(num))

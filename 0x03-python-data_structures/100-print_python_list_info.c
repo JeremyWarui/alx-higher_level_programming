96dd7f172b1740875749b0b78756ea687ef0e306
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-05 12:55:28 +0000

    Call c function using python

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+* print_python_list_info - prints the info of a python list
+* @p: pointer to the list
+* Return: Void
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", size);
+
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %d\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}

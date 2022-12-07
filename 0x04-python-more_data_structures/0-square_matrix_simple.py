be9ccd4945df2d10c3cddf66c889cb8129e66923
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-07 11:50:13 +0000

    Square of matrix values

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """
+    function thhat return square of all matrix values
+    """
+    if len(matrix) != 0:
+        copy = matrix[:]
+        new_list = []
+        for nlist in matrix:
+            copy = list(map(lambda x: x * x, nlist))
+            new_list.append(copy)
+        return new_list

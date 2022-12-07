ca929cf0b90eb1899aa67c0723f97fee8d333f61
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-07 12:17:45 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,6 +5,6 @@ def search_replace(my_list, search, replace):
     and replace the element in
     the list
     """
-    if len(my_list) != 0:
+    if len(my_list) > 0:
         new_list = list(map(lambda x: replace if x == search else x, my_list))
         return new_list

91b27bd2141e596934015a293ecccb915ed8c488
JeremyWarui <jerywarne7@gmail.com>
2022-12-07 20:34:12 +0300

    Maximum value in the dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,4 +6,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        return max(a_dictionary, key = lambda x:a_dictionary[x])
+        return max(a_dictionary, key=lambda x: a_dictionary[x])

4528d7f17fd36fd8acfd7dac06d0d48619d0c542
JeremyWarui <jerywarne7@gmail.com>
2022-12-12 15:41:23 +0300

    ROman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    total = 0
     digit = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for roman in reversed(roman_string):
-        arabic = digit[roman]
-        total += arabic if total < (arabic * 5) else -arabic
-    return total
+    if roman_string and type(roman_string) is str:
+        total = 0
+        for roman in reversed(roman_string):
+            arabic = digit[roman]
+            total += arabic if total < (arabic * 5) else -arabic
+        return total

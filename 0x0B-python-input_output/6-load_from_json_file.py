e4f0a7a0892d1a0bad878a6f7d7622965fccfd5a
JeremyWarui <jerywarne7@gmail.com>
2023-01-10 21:58:44 +0300

    Fix errors on additem

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     Creates a python obj from JSON file
     """
-    with open(filename, mode="w", encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         return json.load(f)

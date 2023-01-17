c8c10dc2c41152dad05c54d10bb5ab12d3b14018
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-01-17 12:05:04 +0000

    Str rep function for rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,6 +106,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """ prints [Rectangle] (<id>) <x>/<y> - <width>/<height> """
-        return ("[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+        return "[{}] ({}) {}/{} - {}/{}".format(
             self.__class__.__name__, self.id, self.__x, self.__y,
-            self.__width, self.__height))
+            self.__width, self.__height)

6c2b71629f0078ff8e39dd9810c7967c0ee95986
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-01-20 14:45:49 +0000

    JSON string of dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ Module: Base """
 
 
+import json
+
+
 class Base():
     """
     A base model class
@@ -17,3 +20,12 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ 
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)

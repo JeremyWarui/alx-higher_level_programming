043dd1e25a60a098ef02a23b06b8578302c834e1
JeremyWarui <jerywarne7@gmail.com>
2023-02-14 22:24:45 +0300

    Advanced Tasks: Fix utf8 unicode

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,5 @@
 -- converts first_table table and `name` field in table to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;

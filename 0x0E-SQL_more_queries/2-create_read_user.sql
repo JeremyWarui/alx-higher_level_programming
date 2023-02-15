b6366f7cf221cf85b3f636fe814152eb64c5915c
JeremyWarui <jerywarne7@gmail.com>
2023-02-15 23:02:33 +0300

    CReate DB and User privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
-
 -- Script that creates an user and a database in MySQL server
 -- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;

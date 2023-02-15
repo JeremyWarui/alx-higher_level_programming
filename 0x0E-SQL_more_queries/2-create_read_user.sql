b3b9d820f637f4eb0608c8627af7d1531db6701c
JeremyWarui <jerywarne7@gmail.com>
2023-02-15 22:58:44 +0300

    CReate DB and User privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,5 @@
 -- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';

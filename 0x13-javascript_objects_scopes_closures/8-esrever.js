01c1bcd64818ab6ed05fcefd0ffdc409917c262c
JeremyWarui <jerywarne7@gmail.com>
2023-03-14 22:23:55 +0300

    Number of times an element appears in a list and reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const copy = [];
-  let i = 0;
-  while (i < list.length - 1) {
-    i++;
-  }
+    const copy = [];
+    let i = 0;
+    while (i < list.length - 1) {
+        i++;
+    }
+    // console.log(i);
+    while (i >= 0) {
+        copy.push(list[i]);
+        i--;
+    }
 
-  while (i >= 0) {
-    copy.push(list[i]);
-    i--;
-  }
-
-  return copy;
+    return copy;
 };

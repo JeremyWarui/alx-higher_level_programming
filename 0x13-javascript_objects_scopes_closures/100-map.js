98de7ea9e744adcb0aa00c51548cf88c1a9f7b55
JeremyWarui <jerywarne7@gmail.com>
2023-03-14 22:47:02 +0300

    Map in arrays

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newlist = list.map((x) => x * 2);
+const newlist = list.map((x, idx) => x * idx);
 console.log(list);
 console.log(newlist);

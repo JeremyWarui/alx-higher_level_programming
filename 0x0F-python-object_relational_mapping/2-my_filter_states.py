90107acecd57dd6d75a92cb2c7eeba2c7b43a759
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-17 13:07:50 +0000

    Fix errors and remove host parameter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,14 +9,13 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(port=3306,
-                         host='localhost',
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""
     SELECT * FROM states
-    WHERE name={:s}
+    WHERE name={:s};
     """.format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,12 +10,11 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(port=3306,
-                         host='localhost',
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s", sys.argv[4])
+    cur.execute("SELECT * FROM states WHERE name=%s;", sys.argv[4])
     states = cur.fetchall()
     for state in states:
         print(state)

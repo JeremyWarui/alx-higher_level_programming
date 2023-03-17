f0ea0c1c8c0951b8a71456fcdc7e8a0e89304e0b
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-17 13:21:29 +0000

    Fix error on capital search letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     states = cur.fetchall()
     for state in states:
         print(state)

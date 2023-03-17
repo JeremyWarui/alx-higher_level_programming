1ad35d9c787c2093a2cef0dc3fa5f7d7d07818de
JeremyWarui <jerywarne7@gmail.com>
2023-03-17 19:57:57 +0300

    Order by ID in ascending order

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,13 +7,29 @@ import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE `name`\
+    LIKE 'N%';")
     states = cur.fetchall()
+
     for state in states:
         print(state)
+# if __name__ == '__main__':
+#     db = MySQLdb.connect(user=sys.argv[1],
+#                          passwd=sys.argv[2],
+#                          db=sys.argv[3],
+#                          port=3306)
+#     cur = db.cursor()
+#     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+#     states = cur.fetchall()
+#     for state in states:
+#         print(state)

6b5701387f8fc23b0065127023140847fabcfc6e
JeremyWarui <jerywarne7@gmail.com>
2023-03-17 20:05:50 +0300

    Order by ID in ascending order

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,6 +9,7 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
+                         host='localhost',
                          passwd=sys.argv[2],
                          db=sys.argv[3],
                          port=3306)

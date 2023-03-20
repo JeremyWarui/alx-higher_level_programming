bf309364f31c7200ac01783a1e5c7abc2d9bb73e
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-20 14:04:52 +0000

    Filter by to get specific state from argument

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,14 +10,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).filter_by(name=sys.argv[4]).first()
+    result = session.query(State).filter(State.name==sys.argv[4]).first()
 
     if result:
         print('{:d}'.format(State.id))

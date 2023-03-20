3da9bd5a8b6fcd16b072e8abeef2f52895e94eb0
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-20 15:21:35 +0000

    Update state where id=2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Change the name of the State where id = 2 to New Mexico
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
+    session.commit()

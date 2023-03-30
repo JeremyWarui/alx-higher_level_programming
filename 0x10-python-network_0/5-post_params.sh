24228e7c945e9d4270ed17cdeba4352a8905b831
JeremyWarui <jerywarne7@gmail.com>
2023-03-30 21:35:55 +0300

    POST request with params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #POST request with parameters
-curl -sI -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
+curl "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"

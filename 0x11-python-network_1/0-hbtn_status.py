05c974254bcc547a80f9d1c24a20f9e8428aa93d
JeremyWarui <jerywarne7@gmail.com>
2023-03-31 22:25:03 +0300

    Fetches url status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as res:
+        response = res.read()
+        print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(response),
+                                 response, response.decode('utf-8')))

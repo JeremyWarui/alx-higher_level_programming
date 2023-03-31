2f451a5469aad1fab14eab35ba4a50fad69dfc2c
JeremyWarui <jerywarne7@gmail.com>
2023-03-31 23:43:37 +0300

    Display contents

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with requests.get(url) as res:
         print("Body response:")
-        print("\t- type: {}".format(type(res)))
-        print("\t- content: {}".format(res))
-        print("\t- utf8 content: {}".format(res.decode('utf-8')))
+        print("\t- type: {}".format(type(res.text)))
+        print("\t- content: {}".format(res.content))

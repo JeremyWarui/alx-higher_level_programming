994b85da799066bf04ea26aa0f785c18e85edaa9
JeremyWarui <jerywarne7@gmail.com>
2023-03-31 23:34:47 +0300

    Display contents

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     with requests.get(url) as res:
         print("Body response:")
         print("\t- type: {}".format(type(res)))
-        print("\t-content: {}".format(res))
-        print("\t-utf8 content: {}".format(res.decode('utf-8')))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))

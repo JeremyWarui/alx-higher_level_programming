f3964c16f7867244a92e3cea5c3ddd5790079b1c
JeremyWarui <jerywarne7@gmail.com>
2023-03-31 23:50:16 +0300

    Display X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that sends request to url and
+displays 'X-Request-Id'
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    with requests.get(url) as res:
+        print(res.headers['X-Request-Id'])

2b42237e05cc926ccb1f6eb4df073cadf9b877d8
JeremyWarui <jerywarne7@gmail.com>
2023-04-26 00:23:49 +0300

    Completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  if (err) console.log(err);
+  else {
+    const obj = {};
+    const body = JSON.parse(res);
+    for (const i in body) {
+      if (body[i].completed === true) {
+        if (obj[body[i].userId] === undefined) {
+          obj[body[i].userId] = 0;
+        }
+        obj[body[i].userId]++;
+      }
+    }
+    console.log(obj);
+  }
+});

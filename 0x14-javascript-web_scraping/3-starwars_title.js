c561eecd3643cb73442a21a57f88fe589115eff3
JeremyWarui <jerywarne7@gmail.com>
2023-04-25 16:08:58 +0000

    Get episode titles of Star wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
-request(url, (err, data) => {
+request(url, (err, body) => {
   if (err) console.log(err);
   else {
-    const body = JSON.parse(data);
-    console.log(body.title);
+    const data = JSON.parse(body.body);
+    console.log(data.title);
   }
 });

574edd88d3b2d2dd9d31895b1000bda4b23901d3
JeremyWarui <jerywarne7@gmail.com>
2023-05-03 23:26:47 +0300

    Advanced tasks

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,18 @@
+const $ = window.$;
+window.onload = function () {
+  const langCode = $('INPUT#language_code');
+  const translate = $('INPUT#btn_translate');
+  const hello = $('DIV#hello');
+  translate.click(function () {
+    $.ajax({
+      url: `https://www.fourtonfish.com/hellosalut?lang=${langCode.val()}`,
+      type: 'GET',
+      dataType: 'json'
+    })
+      .done(function (response) {
+        translate.click(function () {
+          hello.text(response.hello);
+        });
+      });
+  });
+};
